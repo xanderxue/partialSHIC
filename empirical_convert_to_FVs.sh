c9685c10c9bf2d4d4ec941cccdb2baa310788e7e
xanderxue <xanderxue@gmail.com>
2020-03-27 19:05:18 -0400

    Update empirical_convert_to_FVs.sh

--- empirical_convert_to_FVs.sh
+++ empirical_convert_to_FVs.sh
@@ -17,7 +17,7 @@ for pop in AOM BFM BFS CMS GAS GNS GWA KES UGS
     segmentEnd=$(( $segmentStart - 1 + $stepSize + $subWinSize * ($numSubWins - 1) ))
     while [ $segmentStart -lt ${chrLens[$i]} ]
       do
-      python2 empirical_convert_to_FVs.py empiricalData/ag1000g.phase1.ar3.haplotypes.$chrArm.h5 $chrArm ${chrLens[$i]} $segmentStart $segmentEnd $subWinSize $numSubWins 0.25 `python2 mosquito_data_files/stairwayPlotToPMisPol.py mosquito_data_files/$pop.meru_mela.sfs.sp.summary` mosquito_data_files/${pop}_partial_stats.txt mosquito_data_files/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa mosquito_data_files/anc.meru_mela.$chrArm.fa mosquito_data_files/samples_pops.txt $pop empiricalData/sumstats/$pop/$chrArm.$segmentStart.stats empiricalData/FVs/$pop/$chrArm.$segmentStart.fvec > empiricalData/logs/$pop/$chrArm.$segmentStart.log
+      python2 empirical_convert_to_FVs.py empiricalData/ag1000g.phase1.ar3.1.haplotypes.$chrArm.h5 $chrArm ${chrLens[$i]} $segmentStart $segmentEnd $subWinSize $numSubWins 0.25 `python2 mosquito_data_files/stairwayPlotToPMisPol.py mosquito_data_files/$pop.meru_mela.sfs.sp.summary` mosquito_data_files/${pop}_partial_stats.txt mosquito_data_files/Anopheles-gambiae-PEST_CHROMOSOMES_AgamP3.accessible.fa mosquito_data_files/anc.meru_mela.$chrArm.fa mosquito_data_files/samples_pops.txt $pop empiricalData/sumstats/$pop/$chrArm.$segmentStart.stats empiricalData/FVs/$pop/$chrArm.$segmentStart.fvec > empiricalData/logs/$pop/$chrArm.$segmentStart.log
       segmentStart=$(( $segmentStart + $stepSize ))
       segmentEnd=$(( $segmentEnd + $stepSize ))
     done
